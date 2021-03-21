df1b066518c73632b62df586fd2629ee0799b0d6
DaTaylorSeries <zippytato@gmail.com>
2021-03-20 22:52:06 -0600

    3/20/21: Commit

--- JS/Tertiary/Amortize Script.js
+++ JS/Tertiary/Amortize Script.js
@@ -86,3 +86,17 @@ function Calculate() {
       document.getElementById("Calculate").click();
     }
   });
+
+  /* CMD or CTRL + P detection */
+  $(document).keydown(function(e) {
+    if((e.metaKey || e.ctrlKey) && e.key === 'p') {
+      document.getElementById("Print").click();
+  }
+  });
+
+  /* CMD or CTRL + Backspace detection */
+  $(document).keydown(function(e) {
+    if((e.metaKey || e.ctrlKey) && e.keyCode == 8) {
+      document.getElementById("Remove").click();
+  }
+  });
