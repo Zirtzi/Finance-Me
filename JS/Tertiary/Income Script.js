323a9e885059d58ee498b74f2e85b82829934132
DaTaylorSeries <zippytato@gmail.com>
2021-04-02 18:21:01 -0600

    4/2/21: Updates

--- JS/Tertiary/Income Script.js
+++ JS/Tertiary/Income Script.js
@@ -1,57 +1,84 @@
   /* Cascading drop down selection */
   var frequencyObject = {
     "Annually": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     },
     "Semi-Annually": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     },
     "Quarterly": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     },
     "Monthly": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     },
     "Bi-Monthly": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     },
     "Weekly": {
-      "Tax-Exempt": ["Charity"],
-      "Non Tax-Exempt": ["Self-Employed", "Employed",], 
+      "Tax-Exempt": [],
+      "Non Tax-Exempt": [], 
     }
   }
   window.onload = function() {
     var frequencySel = document.getElementById("frequency");
     var statusSel = document.getElementById("status");
-    var employmentSel = document.getElementById("employment");
     for (var x in frequencyObject) {
       frequencySel.options[frequencySel.options.length] = new Option(x, x);
     }
     frequencySel.onchange = function() {
       //empty employments- and status- dropdowns
-      employmentSel.length = 1;
       statusSel.length = 1;
       //display correct values
       for (var y in frequencyObject[this.value]) {
         statusSel.options[statusSel.options.length] = new Option(y, y);
       }
     }
-    statusSel.onchange = function() {
-      //empty employments dropdown
-      employmentSel.length = 1;
-      //display correct values
-      var z = frequencyObject[frequencySel.value][this.value];
-      for (var i = 0; i < z.length; i++) {
-        employmentSel.options[employmentSel.options.length] = new Option(z[i], z[i]);
-      }
-    }
   }
 
+  /* Individual Taxes Function */
+  function individualTaxes(grossIncome, rate) {
+    var grossIncome;
+    var rate;
+    var indRate = rate / 100;
+    var indTax = parseFloat(grossIncome) * (parseFloat(indRate));
+    return indTax.toFixed(2);
+  }
+
+  /* Total Taxes function */
+  function totalTaxes(federal, state, other) {
+    var federal;
+    var state;
+    var other;
+    var total = parseFloat(federal) + parseFloat(state) + parseFloat(other);
+    var totalFinal = total / 100;
+    return totalFinal.toFixed(2);
+  }
+
+  /* Gross Income Function */
+  function grossIncome(weeklyHours, overtimeHours, rate) {
+    var weeklyHours;
+    var overtimeHours;
+    var rate;
+    var overtimeRate = rate * 1.5;
+    var gross = (weeklyHours * rate) + (overtimeRate * overtimeHours);
+    return gross.toFixed(2);
+  }
+
+  /* Net Income Function */
+  function netIncome(gross, taxes) {
+    var gross;
+    var taxes;
+    var net = parseFloat(gross) * (1 - parseFloat(taxes));
+    var totalNet = net.toFixed(2);
+    return totalNet;
+  }
+  
   
   /* Function to calculate income */
   function Calculate() {
@@ -60,44 +87,169 @@
       document.getElementById("Calculate").style.visibility = "hidden";
       // Get info from selection and input fields
       var freq = document.getElementById("frequency").value;
+      var status = document.getElementById("status").value;
       var fed = document.getElementById("Federal").value;
       var state = document.getElementById("State").value;
       var other = document.getElementById("Other").value;
       var hoursPer = document.getElementById("HoursPer").value;
       var overtimePer = document.getElementById("OvertimePer").value;
-      var gross = '-';
-      var net = '-';
+      var rate = document.getElementById("Rate").value;
+      var gross;
+      var net;
       var table = document.getElementById("Income");
-      // Switch statement
+      // Frequency switch statement
       switch (freq) {
         case "Annually":
-          gross = "Hello";
-          net = "Good Bye";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 52);
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 52);
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         case "Semi-Annually":
-          gross = "Yo";
-          net = "C-ya";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 26);
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 26);
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         case "Quarterly":
-          gross = "Heya";
-          net = "Bye";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 13);
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 13);
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         case "Monthly":
-          gross = "Hi";
-          net = "Buh-Bye";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 4.33);
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 4.33);
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         case "Bi-Monthly":
-          gross = "Whats Up";
-          net = "Later";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 2);
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate) * 2);
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         case "Weekly":
-          gross = "Heyyyy";
-          net = "Bye Fucker";
+          if (status == "Tax-Exempt") {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate));
+            var netTotal = grossTotal;
+            gross = "$" + grossTotal;
+            fed = "$0";
+            state = "$0";
+            other = "$0";
+            net = "$" + netTotal;
+          }
+          else {
+            var grossTotal = parseFloat(grossIncome(hoursPer, overtimePer, rate));
+            var fedTaxes = parseFloat(individualTaxes(grossTotal, fed));
+            var stateTaxes = parseFloat(individualTaxes(grossTotal, state));
+            var otherTaxes = parseFloat(individualTaxes(grossTotal, other));
+            var taxesTotal = parseFloat(totalTaxes(fed, state, other));
+            var netTotal = netIncome(grossTotal, taxesTotal);
+            gross = "$" + grossTotal.toFixed(2);
+            fed = "$" + fedTaxes.toFixed(2);
+            state = "$" + stateTaxes.toFixed(2);
+            other = "$" + otherTaxes.toFixed(2);
+            net = "$" + netTotal;
+          }
           break;
         default:
           freq = "N/A";
-          gross = "You Didn't";
-          net = "Choose Anything";
+          gross = "";
+          fed = "";
+          state = "";
+          other = "";
+          net = "";
       }
       // Insert a default row from here:
       var Entry = table.insertRow(1);
@@ -113,6 +265,21 @@
       col3.innerHTML = state;
       col4.innerHTML = other;
       col5.innerHTML = net;  
+    }
+
+    /* Disable Taxes on Tax-Exempt */
+    function disableMe() {
+      var status = document.getElementById("status").value;
+      if (status == "Tax-Exempt") {
+        document.getElementById("Federal").disabled = true;
+        document.getElementById("State").disabled = true;
+        document.getElementById("Other").disabled = true;
+      }
+      else {
+        document.getElementById("Federal").disabled = false;
+        document.getElementById("State").disabled = false;
+        document.getElementById("Other").disabled = false;
+      }
     }
   
     /* Function to clear table */
